69035d2544788f3400b4b44ff67b6d2c78839929
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2026-06-30 16:37:03 +0300

    debian: Depend on libelf-dev for build

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -199,7 +199,7 @@ BUILD_PATH = /usr/lib/llvm-android-$(CLANG_VERSION)/bin
 # Extra packages to add to the Build-Depends section. Mainline builds
 # can have this section empty, unless cross-building.
 # The default is enough to install the Android toolchain, including clang.
-DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross
+DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross, libelf-dev
 
 # Where we're building on
 DEB_BUILD_ON = amd64
